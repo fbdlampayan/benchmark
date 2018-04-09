241aa6ca21a18ae0c519c0179deb8ba39125c867
fbdlampayan <fbdlampayan@yahoo.com>
2018-04-09 15:32:27 +0200

    simple trigger call implementation.

--- BenchmarkTrigger/src/main/java/com/fbdl/grpc/benchmarktrigger/TriggerClient.java
+++ BenchmarkTrigger/src/main/java/com/fbdl/grpc/benchmarktrigger/TriggerClient.java
@@ -5,10 +5,40 @@
  */
 package com.fbdl.grpc.benchmarktrigger;
 
+import com.fbdl.benchmark.grpc.SimRequest;
+import com.fbdl.benchmark.grpc.SimResponse;
+import com.fbdl.benchmark.grpc.SmServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
 /**
  *
  * @author fbdl
  */
 public class TriggerClient {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("client start");
+        
+        String ip = "ndac-ems";
+        int port = 8081;
+        
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext(true).build();
+        SmServiceGrpc.SmServiceBlockingStub clientBlocking = SmServiceGrpc.newBlockingStub(channel);
+        
+        try {
+            SimRequest request = SimRequest.newBuilder().setName("name from client").setImsi("imsi from client").build();
+            SimResponse response = clientBlocking.provisionSim(request);
+            System.out.println("response: " + response.toString());
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            channel.shutdown();
+        }
+    }
     
 }
